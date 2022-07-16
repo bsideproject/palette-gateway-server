7651e410f1dc90a33c2ffe089fc7ad95dd0f0b6d
WooCheol <jikimee64@gmail.com>
2022-07-16 21:11:17 +0900

    Feat: 인증, api 서버 라우팅 설정, Netty 액세스 로그 활성화

--- src/main/java/com/palette/PaletteApplication.java
+++ src/main/java/com/palette/PaletteApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaletteApplication {
 
 	public static void main(String[] args) {
+		// Reactor Netty 액세스 로그를 활성화
+		System.setProperty("reactor.netty.http.server.accessLogEnabled", "true");
 		SpringApplication.run(PaletteApplication.class, args);
 	}
 
