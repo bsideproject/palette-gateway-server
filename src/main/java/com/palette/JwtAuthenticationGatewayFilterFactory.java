6cd845b498c9ce6b30e6aa7460aecef63ae2305a
WooCheol <jikimee64@gmail.com>
2022-08-01 08:31:37 +0900

    Refactor: jwt 게이트웨이 필터 리펙토링
    - graphql 요청일경우 토큰검사 진행
    - rest api 요청이며 refresh 토큰을 이용한 토큰연장 API경우 refresh 토큰검사 추가

--- src/main/java/com/palette/JwtAuthenticationGatewayFilterFactory.java
+++ src/main/java/com/palette/JwtAuthenticationGatewayFilterFactory.java
@@ -1,26 +1,30 @@
 package com.palette;
 
 import com.palette.JwtAuthenticationGatewayFilterFactory.Config;
+import com.palette.infra.jwt.JwtTokenType;
 import com.palette.infra.jwt.JwtUtils;
 import java.nio.charset.StandardCharsets;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @Component
 public class JwtAuthenticationGatewayFilterFactory extends AbstractGatewayFilterFactory<Config> {
+
+    private static final String GRAPHQL_URL = "graphql";
+    private static final String TOKEN_EXTENSION_URL = "token";
+    private static final String REFRESH_TOKEN_COOKIE_NAME = "PTOKEN_REFRESH";
 
     private final JwtUtils jwtUtils;
 
@@ -29,23 +33,50 @@ public class JwtAuthenticationGatewayFilterFactory extends AbstractGatewayFilter
         this.jwtUtils = jwtUtils;
     }
 
+
     @Override
     public GatewayFilter apply(Config config) {
         return ((exchange, chain) -> {
             log.info("JwtAuthenticationGatewayFilterFactory");
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
-//            if (!containsAuthorization(request)) {
-//                return onError(response, "missing authorization header", HttpStatus.FORBIDDEN);
-//            }
-            //String token = extractToken(request);
-//            if (!jwtUtils.validateToken(token, JwtTokenType.ACCESS_TOKEN)) {
-//                return onError(response, "invalid authorization header", HttpStatus.FORBIDDEN);
-//            }
+            String token = extractToken(request);
+
+            String path = request.getURI().getPath();
+            if (GRAPHQL_URL.equals(path)) {
+                if (!containsAuthorization(request)) {
+                    return onError(response, "missing authorization header",
+                        HttpStatus.UNAUTHORIZED);
+                }
+
+                if (!jwtUtils.validateToken(token, JwtTokenType.ACCESS_TOKEN)) {
+                    return onError(response, "invalid authorization header",
+                        HttpStatus.UNAUTHORIZED);
+                }
+            } else {
+                if (TOKEN_EXTENSION_URL.equals(path)) {
+                    String cookieName = getRefreshTokenByCookie(request);
+                    if (StringUtils.hasText(cookieName) && REFRESH_TOKEN_COOKIE_NAME.equals(
+                        cookieName)) {
+                        if (!jwtUtils.validateToken(token, JwtTokenType.REFRESH_TOKEN)) {
+                            return onError(response, "invalid authorization header",
+                                HttpStatus.UNAUTHORIZED);
+                        }
+                    }
+                }
+            }
+
             return chain.filter(exchange);
         });
     }
 
+    private String getRefreshTokenByCookie(ServerHttpRequest request) {
+        HttpCookie cookie = request.getCookies().getFirst(REFRESH_TOKEN_COOKIE_NAME);
+        if (cookie != null) {
+            return cookie.getName();
+        }
+        return "";
+    }
 
     private boolean containsAuthorization(ServerHttpRequest request) {
         return request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION);
@@ -62,10 +93,7 @@ public class JwtAuthenticationGatewayFilterFactory extends AbstractGatewayFilter
         return response.writeWith(Mono.just(buffer));
     }
 
-    @NoArgsConstructor
-    @AllArgsConstructor
     @Getter
-    @Setter
     public static class Config {
 
         private String baseMessage;
