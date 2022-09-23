df429932ea709d095b74d0eb50600f5d8832b553
WooCheol <jikimee64@gmail.com>
2022-09-24 00:53:03 +0900

    Fix: 운영 CI / CD 테스트

--- src/main/java/com/palette/PaletteApplication.java
+++ src/main/java/com/palette/PaletteApplication.java
@@ -2,14 +2,16 @@ package com.palette;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PaletteApplication {
 
-	public static void main(String[] args) {
-		// Reactor Netty 액세스 로그를 활성화
-		System.setProperty("reactor.netty.http.server.accessLogEnabled", "true");
-		SpringApplication.run(PaletteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Reactor Netty 액세스 로그를 활성화
+        System.setProperty("reactor.netty.http.server.accessLogEnabled", "true");
+        SpringApplication.run(PaletteApplication.class, args);
+    }
 
 }
